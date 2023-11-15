d8e62d657ad6adc1b21aaedbeb1bcc5c02a08c4a
kasa021 <mkafa.k11@gmail.com>
2023-11-15 14:39:54 +0900

    グリットの状態を一つの変数で管理するように修正

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Box from "@mui/material/Box";
 
-function Square() {
-  const [value, setValue] = useState<string>("");
-  const handleClick = () => {
-    value === "" ? setValue("X") : setValue("");
-  }
+function Square({
+  value,
+  onClickSquare,
+}: {
+  value: string;
+  onClickSquare: () => void;
+}) {
+
   return (
     <Button
       sx={{
         width: "60px",
         height: "60px",
       }}
-      onClick={handleClick}
+      onClick = {onClickSquare}
     >
       {value}
     </Button>
@@ -23,7 +26,13 @@ function Square() {
 }
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [squares, setSquares] = useState<string[]>(Array(9).fill(""));
+
+  const handleClick = (i: number) => {
+    const squaresCopy = [...squares]; 
+    squaresCopy[i] = "X";
+    setSquares(squaresCopy);
+  }
 
   return (
     <>
@@ -35,19 +44,19 @@ function App() {
         }}
       >
         <ButtonGroup variant="outlined" aria-label="outlined button group">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[0]} onClickSquare={() => handleClick(0)} />
+          <Square value={squares[1]} onClickSquare={() => handleClick(1)} />
+          <Square value={squares[2]} onClickSquare={() => handleClick(2)} />
         </ButtonGroup>
         <ButtonGroup variant="outlined" aria-label="text button group">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[3]} onClickSquare={() => handleClick(3)} />
+          <Square value={squares[4]} onClickSquare={() => handleClick(4)} />
+          <Square value={squares[5]} onClickSquare={() => handleClick(5)} />
         </ButtonGroup>
         <ButtonGroup variant="outlined" aria-label="text button group">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[6]} onClickSquare={() => handleClick(6)} />
+          <Square value={squares[7]} onClickSquare={() => handleClick(7)} />
+          <Square value={squares[8]} onClickSquare={() => handleClick(8)} />
         </ButtonGroup>
       </Box>
     </>
