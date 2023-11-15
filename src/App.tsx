e4c879e4a06e5583e5b62b44683ef8bda590e851
kasa021 <mkafa.k11@gmail.com>
2023-11-15 15:13:54 +0900

    勝敗を決める機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,37 @@ function Square({
   value: string;
   onClickSquare: () => void;
 }) {
-
   return (
     <Button
       sx={{
         width: "60px",
         height: "60px",
       }}
-      onClick = {onClickSquare}
+      onClick={onClickSquare}
     >
       {value}
     </Button>
   );
+}
+
+function calculateWinner(squares: string[]) {
+  const lines: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 function App() {
@@ -30,15 +49,24 @@ function App() {
   const [squares, setSquares] = useState<string[]>(Array(9).fill(""));
 
   const handleClick = (i: number) => {
-    if(squares[i] !== "") return;
-    const squaresCopy = [...squares]; 
-    xIsNext ? squaresCopy[i] = "X" : squaresCopy[i] = "O";
+    if (squares[i] !== "" || calculateWinner(squares)) return;
+    const squaresCopy = [...squares];
+    xIsNext ? (squaresCopy[i] = "X") : (squaresCopy[i] = "O");
     setSquares(squaresCopy);
     setXIsNext(!xIsNext);
+  };
+
+  const winner = calculateWinner(squares);
+  let status: string;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   return (
     <>
+      <div>{status}</div>
       <Box
         sx={{
           display: "flex",
